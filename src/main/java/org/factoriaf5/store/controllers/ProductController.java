b455e88bf198525bec5f79ff2674b9058e14bcd9
mavieuwu <mabigaming26@gmail.com>
2021-05-17 10:33:51 +0200

    agregar metodo check y return hello im a server

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -1,4 +1,16 @@
 package org.factoriaf5.store.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+
+
+@RestController
 public class ProductController {
+    @GetMapping("/health")
+    public String check() {
+        return "Hello! I'm a server.";
+    }
 }
